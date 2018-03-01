9ce7c3e64c917bee5af5f816730c9be049b08e7f
Pablo <pjorda96@gmail.com>
2018-03-01 17:34:22 +0100

    menuda rallada colega

--- 06-Shell Scripts/Actividad6a/Actividad6a-6.sh
+++ 06-Shell Scripts/Actividad6a/Actividad6a-6.sh
@@ -1,15 +1,21 @@
-read -p "Introduce una cantidad" x
+read -p "Introduce una cantidad: " x
 
 precio=0
 
-if [ $x -lt 50 ]; then
+if [ $x -le 50 ]; then
     precio=20
 else
     precio=20
-    x=$x - 50
+    x=$(echo "scale=2; $x - 50" | bc)
 
-    if [ $x -lt 20 ]; then
-	precio=$precio + 20
+    if [ $x -le 200 ]; then
+	precio=$(echo "scale=2; $precio + 0.20 * $x" | bc)
     else
-	precio=$precio + 20
-	x=$x - 200
+	precio=$(echo "scale=2; $precio + 40" | bc)
+	x=$(echo "scale=2; $x - 200" | bc)
+
+        precio=$(echo "scale=2; $precio + 0.10 * $x" | bc)
+    fi
+fi
+
+echo $precio
